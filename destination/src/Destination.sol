7ccac4c4fed8f6565a7fe07b3adcf1e573a3f848
Chutengli <72440194+Chutengli@users.noreply.github.com>
2025-11-10 20:24:26 -0600

    done

--- destination/src/Destination.sol
+++ destination/src/Destination.sol
@@ -23,15 +23,48 @@ contract Destination is AccessControl {
     }
 
 	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
-		//YOUR CODE HERE
+		address wrapped_token_address = wrapped_tokens[_underlying_token];
+		require(wrapped_token_address != address(0), "Token not registered");
+		require(_recipient != address(0), "Invalid recipient address");
+		require(_amount > 0, "Amount must be greater than zero");
+		
+		BridgeToken wrapped_token = BridgeToken(wrapped_token_address);
+		wrapped_token.mint(_recipient, _amount);
+		
+		emit Wrap(_underlying_token, wrapped_token_address, _recipient, _amount);
 	}
 
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
-		//YOUR CODE HERE
+		address underlying_token_address = underlying_tokens[_wrapped_token];
+		require(underlying_token_address != address(0), "Token not registered");
+		require(_recipient != address(0), "Invalid recipient address");
+		require(_amount > 0, "Amount must be greater than zero");
+		
+		BridgeToken wrapped_token = BridgeToken(_wrapped_token);
+		require(wrapped_token.balanceOf(msg.sender) >= _amount, "Insufficient balance");
+		
+		wrapped_token.burnFrom(msg.sender, _amount);
+		
+		emit Unwrap(underlying_token_address, _wrapped_token, msg.sender, _recipient, _amount);
 	}
 
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
-		//YOUR CODE HERE
+		require(_underlying_token != address(0), "Invalid underlying token address");
+		require(wrapped_tokens[_underlying_token] == address(0), "Token already created");
+		
+		BridgeToken wrapped_token = new BridgeToken(_underlying_token, name, symbol, address(this));
+		address wrapped_token_address = address(wrapped_token);
+		
+		wrapped_tokens[_underlying_token] = wrapped_token_address;
+		underlying_tokens[wrapped_token_address] = _underlying_token;
+		tokens.push(wrapped_token_address);
+		
+		// Note: MINTER_ROLE is already granted to address(this) by the BridgeToken constructor
+		// since address(this) is passed as the admin parameter
+		
+		emit Creation(_underlying_token, wrapped_token_address);
+		
+		return wrapped_token_address;
 	}
 
 }
